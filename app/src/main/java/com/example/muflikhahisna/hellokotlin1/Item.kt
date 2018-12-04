8cb9d5c9b846442ad7b712ab047ad6d74fcddd09
muflikhah isna <muflikhah.a@students.amikom.ac.id>
2018-12-04 16:06:14 +0700

    Commit_2

---
+++ app/src/main/java/com/example/muflikhahisna/hellokotlin1/Item.kt
@@ -0,0 +1,3 @@
+package com.example.muflikhahisna.hellokotlin1
+
+data class Item (val name: String?, val image: Int?)
--- app/src/main/java/com/example/muflikhahisna/hellokotlin1/MainActivity.kt
+++ app/src/main/java/com/example/muflikhahisna/hellokotlin1/MainActivity.kt
@@ -2,17 +2,37 @@ package com.example.muflikhahisna.hellokotlin1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
-
+    private var items: MutableList<Item> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
+        val list = findViewById<RecyclerView>(R.id.club_list)
+        initData()
+
+        list.layoutManager = LinearLayoutManager(this)
+        list.adapter = RecyclerViewAdapter(this, items)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textHello = findViewById<TextView>(R.id.text_hello)
         textHello.text = "Hello Kotlin!"
+
     }
 
+    private fun initData(){
+        val name = resources.getStringArray(R.array.club_name)
+        val image = resources.obtainTypedArray(R.array.club_image)
+        items.clear()
+        for (i in name.indices) {
+            items.add(Item(name[i],
+                    image.getResourceId(i, 0)))
+        }
+
+        //Recycle the typed array
+        image.recycle()
+    }
 
 }
